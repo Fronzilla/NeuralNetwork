35bdfa84d8324183517ea62fd71f38911397aafa
Alexey Nikitin <abuseofinnocent@gmail.com>
2018-07-30 00:24:26 +0700

    Some files

--- Boston house prices prediction.py
+++ Boston house prices prediction.py
@@ -1,20 +1,13 @@
-
 # coding: utf-8
 
-# In[16]:
-
-
 from keras.datasets import boston_housing
-from keras import models,layers
+from keras import models, layers
 import numpy as np
 import matplotlib.pyplot as plt
+
 (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
-
-# In[3]:
-
-
-#data normalisation
+# data normalisation
 mean = train_data.mean(axis=0)
 train_data -= mean
 std = train_data.std(axis=0)
@@ -23,23 +16,17 @@ test_data -= mean
 test_data /= std
 
 
-# In[4]:
-
-
 def build_model():
     model = models.Sequential()
-    model.add(layers.Dense(64, activation = 'relu',
-             input_shape =(train_data.shape[1],)))
-    model.add(layers.Dense(64, activation = 'relu'))
+    model.add(layers.Dense(64, activation='relu',
+                           input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1))
     model.compile(
-            optimizer='rmsprop',
-            loss='mse',
-            metrics =['mae'])
+        optimizer='rmsprop',
+        loss='mse',
+        metrics=['mae'])
     return model
-
-
-# In[5]:
 
 
 # K-fold cross-validation
@@ -49,134 +36,84 @@ num_epochs = 100
 all_scores = []
 
 for i in range(k):
-    print('proccesing fold #',i)
+    print('proccesing fold #', i)
     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    val_targets = train_targets[i * num_val_samples: (i+1) * num_val_samples]
-    
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+
     partial_train_data = np.concatenate(
-    [train_data[:i * num_val_samples],
-     train_data[(i + 1) * num_val_samples:]],
-    axis=0)
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
     partial_train_targets = np.concatenate(
-    [train_targets[:i *num_val_samples],
-     train_targets[(i + 1)*num_val_samples:]],
-    axis=0)
-
-
-# In[6]:
-
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
 
 model = build_model()
 
-
-# In[7]:
-
-
 model.fit(partial_train_data, partial_train_targets,
-         epochs = num_epochs, batch_size=1, verbose=0
-         )
+          epochs=num_epochs, batch_size=1, verbose=0
+          )
 
-
-# In[8]:
-
-
-#valiation
-val_mse, val_mae = model.evaluate(val_data, val_targets, verbose =0)
+# validation
+val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
 all_scores.append(val_mae)
-
-
-# In[10]:
-
 
 # saving evaluating score during each epoch
 num_epochs = 500
 all_mae_history = []
 for i in range(k):
     print('processing fold #', i)
-    val_data = train_data [i * num_val_samples: (i + 1) * num_val_samples]
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-    
+
     partial_train_data = np.concatenate(
-    [train_data[:i * num_val_samples],
-     train_data[(i + 1) * num_val_samples:]],
-    axis=0)
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
     partial_train_targets = np.concatenate(
-    [train_targets[:i *num_val_samples],
-     train_targets[(i + 1)*num_val_samples:]],
-    axis=0)
-    
-
-
-# In[11]:
-
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
 
 model = build_model()
 
-
-# In[13]:
-
-
-history = model.fit(partial_train_data, partial_train_targets, validation_data=(val_data, val_targets), 
-                   epochs=num_epochs, batch_size=1, verbose=0)
+history = model.fit(partial_train_data, partial_train_targets, validation_data=(val_data, val_targets),
+                    epochs=num_epochs, batch_size=1, verbose=0)
 mae_history = history.history['val_mean_absolute_error']
 all_mae_history.append(mae_history)
 
-
-# In[14]:
-
-
-# the avarage score history
+# the average score history
 average_mae_history = [
-    np.mean([x[i] for x in all_mae_history]) for i in range (num_epochs)
+    np.mean([x[i] for x in all_mae_history]) for i in range(num_epochs)
 ]
-
-
-# In[18]:
-
-
 # graph with scores validations
-plt.plot(range(1, len(average_mae_history) +1 ), average_mae_history)
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
 plt.xlabel('Epohcs')
 plt.ylabel("Validation MAE")
 plt.show()
 
 
-# In[20]:
-
-
-# graph with scores validations except first 10 
+# graph with scores validations except first 10
 def smooth_curve(points, factor=0.9):
     smoothed_points = []
     for i in points:
         if smoothed_points:
             previous = smoothed_points[-1]
-            smoothed_points.append(previous * factor * i *(1-factor))
+            smoothed_points.append(previous * factor * i * (1 - factor))
         else:
             smoothed_points.append(i)
     return smoothed_points
+
+
 smooth_mae_history = smooth_curve(average_mae_history[10:])
-
-
-# In[22]:
-
 
 plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
 plt.xlabel("Epochs")
 plt.ylabel("Validation MAE")
 plt.show()
 
-
-# In[23]:
-
-
 # final network teaching
 model = build_model()
 model.fit(train_data, train_targets, epochs=80, batch_size=16, verbose=0)
 test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
-
-
-# In[25]:
-
-
-
-
