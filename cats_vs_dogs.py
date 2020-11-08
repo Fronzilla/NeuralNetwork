c88da1a5ad841a2fc1c8154a0d69d8ea3fae402a
Alexey Nikitin <abuseofinnocent@gmail.com>
2020-11-08 22:28:22 +0700

    typos

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -1,17 +1,16 @@
-
 # coding: utf-8
 
 import os, shutil
 from keras import layers, models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from keras.preprocessing import image
 
-original_dataset_dir = '/Users/alexeynikitin/Desktop/datasets/all/kaggle_original_data'
-base_dir = '/Users/alexeynikitin/Desktop/datasets/all/cats_and_dogs_small'
+original_dataset_dir = '/kaggle_original_data'
+base_dir = '/cats_and_dogs_small'
 
-#creating catalogs
+# creating catalogs
 train_dir = os.path.join(base_dir, 'train')
 os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir, 'validation')
@@ -19,8 +18,7 @@ os.mkdir(validation_dir)
 test_dir = os.path.join(base_dir, 'test')
 os.mkdir(test_dir)
 
-
-#catalogs
+# catalogs
 train_cats_dir = os.path.join(train_dir, 'cats')
 os.mkdir(train_cats_dir)
 
@@ -39,94 +37,88 @@ os.mkdir(test_cats_dir)
 test_dogs_dir = os.path.join(test_dir, 'dogs')
 os.mkdir(test_dogs_dir)
 
-
-#coping first 1000 cats images
-fnames = ['cat.{}.jpg'.format(i) for i in range (1000)]
+# coping first 1000 cats images
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_cats_dir, fname)
     shutil.copyfile(scr, dst)
 
-#coping second 500 cats images for validation
-fnames = ['cat.{}.jpg'.format(i) for i in range (1000, 1500)]
+# coping second 500 cats images for validation
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_cats_dir, fname)
     shutil.copyfile(scr, dst)
 
-#coping second 500 cats images for test
-fnames = ['cat.{}.jpg'.format(i) for i in range (1500, 2000)]
+# coping second 500 cats images for test
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_cats_dir, fname)
     shutil.copyfile(scr, dst)
 
-#coping first 1000 dogs images
-fnames = ['dog.{}.jpg'.format(i) for i in range (1000)]
+# coping first 1000 dogs images
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_dogs_dir, fname)
     shutil.copyfile(scr, dst)
 
-
-#coping second 500 dogs images for validation
-fnames = ['cat.{}.jpg'.format(i) for i in range (1000, 1500)]
+# coping second 500 dogs images for validation
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_dogs_dir, fname)
     shutil.copyfile(scr, dst)
 
-
 # coping second 500 cats images for test
-fnames = ['cat.{}.jpg'.format(i) for i in range (1500, 2000)]
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
     scr = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_dogs_dir, fname)
     shutil.copyfile(scr, dst)
 
-
 # convolution network
 model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(150,150,3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
-model.add(layers.Dense(512,activation ='relu'))
-model.add(layers.Dense(1, activation = 'sigmoid'))
-
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
-             optimizer=optimizers.RMSprop(lr=1e-4),
+              optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
 
-
-train_datagen = ImageDataGenerator(rescale = 1./255)
-test_datagen = ImageDataGenerator(rescale = 1./255)
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
-train_dir,
-target_size=(150,150),
-batch_size=20,
-class_mode='binary')
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-validation_dir,
-target_size=(150,150),
-batch_size=20,
-class_mode='binary')
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
 
 history = model.fit_generator(
-train_generator,
-steps_per_epoch=100,
-epochs=30,
-validation_data=validation_generator,
-validation_steps=50)
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
 
 model.save('cats_and_dogs_small_1.h5')
 
@@ -136,21 +128,21 @@ loss = history.history['loss']
 vall_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
-plt.plot(epochs, acc, 'bo', label = 'Training acc')
-plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.title('Training and validation acuracy')
 
 plt.legend()
 plt.figure()
 
 plt.plot(epochs, loss, 'bo', label='Training_loss')
-plt.plot(epochs,vall_loss, 'b', label = 'Validation loss')
+plt.plot(epochs, vall_loss, 'b', label='Validation loss')
 
 plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
-#dealing with overlearning, picture editing
+# dealing with overlearning, picture editing
 datagen = ImageDataGenerator(
     rotation_range=40,
     width_shift_range=0.2,
@@ -160,73 +152,70 @@ datagen = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode='nearest')
 
-
 # In[78]:
 
 
 fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
 img_path = fnames[3]
-img = image.load_img(img_path, target_size(150,150))
+img = image.load_img(img_path, target_size=(150, 150))
 x = image.img_to_array(img)
 
-x= x.reshape((1,) + x.shape)
+x = x.reshape((1,) + x.shape)
 
 i = 0
 for batch in datagen.flow(x, batch_size=1):
     plt.figure(i)
     imgplot = plt.imshow(image.array_to_img(batch[0]))
     i += 1
-    if i % 4 ==0:
+    if i % 4 == 0:
         break
 
 # re - learning model
 
 model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(150,150,3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512,activation ='relu'))
-model.add(layers.Dense(1, activation = 'sigmoid'))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
-             optimizer=optimizers.RMSprop(lr=1e-4),
+              optimizer=optimizers.RMSprop(lr=1e-4),
               metrics=['acc'])
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     rotation_range=40,
     width_shift_range=0.2,
     height_shift_range=0.2,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True,)
-test_datagen = train_datagen = ImageDataGenerator(rescale=1./255
+    horizontal_flip=True, )
+test_datagen = train_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size(150,150),
+    target_size=(150, 150),
     batch_size=32,
     class_mode='binary')
 
-
 train_generator = test_datagen.flow_from_directory(
     validation_dir,
-    target_size(150,150),
+    target_size=(150, 150),
     batch_size=32,
     class_mode='binary')
 
 history = model.fit_generator(
-train_generator,
-steps_per_epoch=100,
-epochs=100,
-validation_data=validation_generator,
-validation_steps=50)
-
+    train_generator,
+    steps_per_epoch=100,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=50)
